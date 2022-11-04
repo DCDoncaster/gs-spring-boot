e631b353d2dabcfea3597145c67328262c6de7d5
David Clarke <david26th@gmail.com>
2022-11-04 15:14:05 +0000

    Changes to gettest

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -1,39 +1,59 @@
 package com.example.springboot;
 
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.google.gson.*;
 
 @RestController
 public class HelloController {
 
-
 	@GetMapping("")
 	public String hello() {
-		return "Hello Doncaster!";
+		return "Hello Team!";
 	}
 
 	@GetMapping("2add2")
 	public int add2() {
-		
+
 		return 2 + 2;
 	}
+
 	@GetMapping("add")
 	public String data(@RequestBody String data) {
 		return data;
 	}
 
-	@RequestMapping(value="squarenumber", method = RequestMethod.GET)
-	public @ResponseBody int getInt(@RequestParam("input") int inputValue){
-				return inputValue * inputValue;
+	@GetMapping("squarenumber")
+	public @ResponseBody double getDouble(@RequestParam("input") double inputValue) {
+		return inputValue * inputValue;
 	}
 
+	@GetMapping("gettest")
+	//send reponsebody with requestbody
+	public @ResponseBody String getTest(@RequestBody String data) {
+		
+	Gson gson = new Gson();
+	Map map = gson.fromJson(data, Map.class);
+	map.get("a").getClass();
+	return map.get("a").toString();
+	
 
+
+	
+		// return data;
+
+	}
 }
